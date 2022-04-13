ce14cdc0ceff098173cbadcc2950c22e34a1cd49
Tomás <tomas.benitez.silva@gmail.com>
2022-04-13 11:01:29 -0300

    added backdrop to mobile nav

--- src/js/components/NavToggler.js
+++ src/js/components/NavToggler.js
@@ -1,13 +1,16 @@
 const NavToggler = function (element) {
   this.toggler = element;
   this.nav = document.querySelector(".js-nav");
+  this.backdrop = document.querySelector(".js-nav-backdrop");
 
   this.handleClick = () => {
     this.nav.classList.toggle("open");
+    this.backdrop.classList.toggle("open");
   };
 
   this.init = () => {
     this.toggler.addEventListener("click", this.handleClick);
+    this.backdrop.addEventListener("click", this.handleClick);
     return this;
   };
 };
